93ee40eac8d4b13048d051890f56392dfb7c7b45
Steven Rho <steven.rho@cashrewards.com>
2018-03-15 18:18:24 +1100

    Now BUILD Number has build metadata.

--- src/main.sh
+++ src/main.sh
@@ -39,13 +39,13 @@ cat $_DIRECTORY/.build_info
 # Parse response and get build number
 if type_exists 'python'
   then
-  echo $_BUILD_INFO | python -c "import sys, json; print json.load(sys.stdin)['item']['buildnumber']" > "$_DIRECTORY/.build_number"  
+  echo $_BUILD_INFO | python -c "import sys, json; print json.load(sys.stdin)['item']['build_metadata']" > "$_DIRECTORY/.build_number"  
 elif type_exists 'php'
   then
-  echo $_BUILD_INFO | php -r '$a =json_decode(file_get_contents("php://stdin"), true); echo $a["item"]["buildnumber"];' > "$_DIRECTORY/.build_number"
+  echo $_BUILD_INFO | php -r '$a =json_decode(file_get_contents("php://stdin"), true); echo $a["item"]["build_metadata"];' > "$_DIRECTORY/.build_number"
 elif type_exists 'jq'
   then
-  echo $_BUILD_INFO | jq -r '.buildnumber' > "$_DIRECTORY/.item.build_number"
+  echo $_BUILD_INFO | jq -r '.buildnumber' > "$_DIRECTORY/.item.build_metadata"
 else
   error "Please install python or PHP"
 fi
